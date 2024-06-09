0e1769f0cd0cc220365ece93f6eae52b3b607d4e
İsmail Kağan Acar <acarismailkagan@gmail.com>
2024-06-09 12:24:20 +0300

    All buttons added and linked to ceiling. Background changed

--- board/static/scripts.js
+++ board/static/scripts.js
@@ -14,13 +14,14 @@ const   Engine = Matter.Engine,
         Vector= Matter.Vector,
         Common = Matter.Common,
         MouseConstraint = Matter.MouseConstraint,
+        Constraint = Matter.Constraint,
         Mouse = Matter.Mouse,
         Svg = Matter.Svg,
         World = Matter.World,
         Vertices = Matter.Vertices,
         Events = Matter.Events; 
 
-const   iEngine = Engine.create({gravity: {y:0}}),
+const   iEngine = Engine.create({gravity: {y:1}}),
         world = iEngine.world;
 
 const   iRunner = Runner.create();
@@ -112,16 +113,16 @@ function party_obj(el) {
 } 
 
 function create_walls() {
-  const wall_bottom = Bodies.rectangle(window.innerWidth/2,window.innerHeight,window.innerWidth,10,{isStatic: true});
-  const wall_top = Bodies.rectangle(window.innerWidth/2,0,window.innerWidth,10,{isStatic: true});
-  const wall_left = Bodies.rectangle(0,window.innerHeight/2,10,window.innerHeight,{isStatic: true});
-  const wall_right = Bodies.rectangle(window.innerWidth,window.innerHeight/2,10,window.innerHeight,{isStatic: true});
+  const wall_bottom = Bodies.rectangle(window.innerWidth/2,window.innerHeight,window.innerWidth,10,{isStatic: true,density:10});
+  const wall_top = Bodies.rectangle(window.innerWidth/2,-10,window.innerWidth,10,{isStatic: true,density:10});
+  const wall_left = Bodies.rectangle(-10,window.innerHeight/2,10,window.innerHeight,{isStatic: true,density:10});
+  const wall_right = Bodies.rectangle(window.innerWidth,window.innerHeight/2,10,window.innerHeight,{isStatic: true,density:10});
   Composite.add(world, [wall_bottom,wall_top,wall_left,wall_right]);
 }
 
 function create_env_interaction_buttons() {
 
-  var button_del_obj = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_del_obj = Bodies.circle(window.innerWidth*0.15,350,30,{
     isStatic: false,
     url: "del_obj",
     restitution: 0.8,
@@ -134,9 +135,9 @@ function create_env_interaction_buttons() {
         yScale:0.2083
       }   
     }
-  });World.add(world, [button_del_obj]);
+  });
 
-  var button_color_obj = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_color_obj = Bodies.circle(window.innerWidth*0.15,150,30,{
     isStatic: false,
     url: "color_obj",
     restitution: 0.8,
@@ -149,9 +150,9 @@ function create_env_interaction_buttons() {
         yScale:0.2083
       }   
     }
-  });World.add(world, [button_color_obj]);
+  });
 
-  var button_gravity_obj = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_gravity_obj = Bodies.circle(window.innerWidth*0.15,250,30,{
     isStatic: false,
     url: "gravity_obj",
     restitution: 0.8,
@@ -164,9 +165,9 @@ function create_env_interaction_buttons() {
         yScale:0.2083
       }   
     }
-  });World.add(world, [button_gravity_obj]);
+  });
 
-  var button_party_obj = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_party_obj = Bodies.circle(window.innerWidth*0.15,50,30,{
     isStatic: false,
     url: "party_obj",
     restitution: 0.8,
@@ -176,14 +177,66 @@ function create_env_interaction_buttons() {
       sprite:{
         texture:"https://media.publit.io/file/party-horn-svgrepo-com.png",
         xScale:0.2083,
-        yScale:0.2083
+        yScale:0.2083,
       }   
     }
-  });World.add(world, [button_party_obj]);
+  });
+
+  link0 = Constraint.create({
+    pointA: {x:window.innerWidth*0.20, y:0},
+    bodyB: button_party_obj,
+    stiffness: 0.1,
+    length:50,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  link1 = Constraint.create({
+    bodyA: button_party_obj,
+    bodyB: button_color_obj,
+    stiffness: 0.1,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  link2 = Constraint.create({
+    bodyA: button_color_obj,
+    bodyB: button_gravity_obj,
+    stiffness: 0.1,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  link3 = Constraint.create({
+    bodyA: button_gravity_obj,
+    bodyB: button_del_obj,
+    stiffness: 0.1,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  World.add(world,[link0,link1,link2,link3]);
+  World.add(world,[button_color_obj,button_del_obj,button_gravity_obj,button_party_obj])
 }
 
 function create_social_buttons() {
-  var button_github = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_github = Bodies.circle(50,50,30,{
     isStatic: false,
     url: "https://github.com/ikaganacar1",
     restitution: 0.8,
@@ -196,9 +249,9 @@ function create_social_buttons() {
         yScale:0.2083
       }   
     }
-  });World.add(world, [button_github]);
+  });
 
-  var button_instagram = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_instagram = Bodies.circle(50,150,30,{
     isStatic: false,
     url: "https://www.instagram.com/ikaganacar/",
     restitution: 0.8,
@@ -211,9 +264,9 @@ function create_social_buttons() {
         yScale:0.2083
       }   
     }
-  });World.add(world, [button_instagram]);
+  });
 
-  var button_x = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_x = Bodies.circle(50,250,30,{
     isStatic: false,
     url: "https://x.com/ikaganacar",
     restitution: 0.8,
@@ -226,9 +279,9 @@ function create_social_buttons() {
         yScale:0.2083
       }   
     }
-  });World.add(world, [button_x]);
+  });
 
-  var button_linkedin = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_linkedin = Bodies.circle(50,350,30,{
     isStatic: false,
     url: "https://www.linkedin.com/in/ismail-kağan-acar-24481b24b/",
     restitution: 0.8,
@@ -241,9 +294,9 @@ function create_social_buttons() {
         yScale:0.2083
       }   
     }
-  });World.add(world, [button_linkedin]);
+  });
 
-  var button_mail = Bodies.circle(window.innerWidth/2,window.innerHeight/2,30,{
+  var button_mail = Bodies.circle(50,450,30,{
     isStatic: false,
     url: "mailto:acarismailkagan@gmail.com",
     restitution: 0.8,
@@ -256,8 +309,73 @@ function create_social_buttons() {
         yScale:0.2083
       }   
     }
-  });World.add(world, [button_mail])
-}
+  });
+
+  link0 = Constraint.create({
+    pointA: {x:window.innerWidth*0.1, y:0},
+    bodyB: button_github,
+    stiffness: 0.1,
+    length:50,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  link1 = Constraint.create({
+    bodyA: button_github,
+    bodyB: button_instagram,
+    stiffness: 0.1,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  link2 = Constraint.create({
+    bodyA: button_instagram,
+    bodyB: button_x,
+    stiffness: 0.1,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  link3 = Constraint.create({
+    bodyA: button_x,
+    bodyB: button_linkedin,
+    stiffness: 0.1,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  link4 = Constraint.create({
+    bodyA: button_linkedin,
+    bodyB: button_mail,
+    stiffness: 0.1,
+    render:{
+      lineWidth:1,
+      type: 'line',
+      anchors: false,
+      strokeStyle: '#393E46'
+    }
+  });
+
+  World.add(world,[link0,link1,link2,link3,link4]);
+  World.add(world,[button_github,button_instagram,button_x,button_linkedin,button_mail,]);
+
+}//end of create social button function
 
 //detect mouse
 let mouse = Mouse.create(iRender.canvas);
