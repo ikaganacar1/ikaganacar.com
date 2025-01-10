95cacbc3a0ed3b227f916eb9b982594134a8d3f8
İsmail Kağan Acar <acarismailkagan@gmail.com>
2025-01-10 04:38:30 +0300

    migration bug fix

--- board/__init__.py
+++ board/__init__.py
@@ -11,10 +11,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.app_context().push()
 application = app
-app.config["SECRET_KEY"] = open("board/secret_key.txt", "r").readline()
+app.config["SECRET_KEY"] = open("board/secret_key.txt", "r").readlines()[0]
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "site.db")
 
-db = SQLAlchemy(app)
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(app, metadata=metadata)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
@@ -23,8 +33,9 @@ from board.routes import (
     admin_routes,
     useless_projects_routes,
     error_routes,
-    apartment_routes
+    apartment_routes,
+    movie_collection_routes
 )
 
 from flask_migrate import Migrate
-migrate = Migrate(app, db)
+migrate = Migrate(app, db,render_as_batch=True)
